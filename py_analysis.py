87b881f5c42ab8ccb8880e9b53b82e86c232b681
Matt Young <young.qubit@gmail.com>
2018-06-07 17:35:15 -0700

    python changes

--- py_analysis.py
+++ py_analysis.py
@@ -1,16 +1,69 @@
 import glob
-import matplot.pyplot as plot
+import matplotlib.pyplot as plot
 import numpy as np
 
-for name in glob.glob('./outputs/case_*'):
+plot_points = []
+iterations = []
+
+def get_iterations(file_name):
+    return file_name.split('_')[len(file_name.split('_')) - 1]
+
+
+for name in glob.glob('./outputs/case_1_count*'):
+    iterations.append(int(get_iterations(name)))
     with open(name) as in_file:
         arr = []
         for line in in_file.readlines():
             arr.append(float(line))
 
-        print(name, 'Avg. time:', np.average(arr))
+        plot_points.append(float(np.average(arr)))
+    plot.plot(iterations, plot_points, '.r-')
+    plot.xscale('log')
+plot_points = []
+iterations = []
 
-# Matplot stuff
-# plot.plot([100, 1000, 10000, 100000, 1000000, 10000000, 100000000], ['''times here'''], 'b-')
-# plot.xscale('log')
-# plot.show()
+
+
+for name in glob.glob('./outputs/case_2_count*'):
+    iterations.append(int(get_iterations(name)))
+    with open(name) as in_file:
+        arr = []
+        for line in in_file.readlines():
+            arr.append(float(line))
+
+        plot_points.append(float(np.average(arr)))
+    plot.plot(iterations, plot_points, '.y-')
+    plot.xscale('log')
+plot_points = []
+iterations = []
+
+
+for name in glob.glob('./outputs/case_3_count*'):
+    iterations.append(int(get_iterations(name)))
+    with open(name) as in_file:
+        arr = []
+        for line in in_file.readlines():
+            arr.append(float(line))
+
+        plot_points.append(float(np.average(arr)))
+    plot.plot(iterations, plot_points, '.b-')
+    plot.xscale('log')
+plot_points = []
+iterations = []
+
+
+for name in glob.glob('./outputs/case_4_count*'):
+    iterations.append(int(get_iterations(name)))
+    with open(name) as in_file:
+        arr = []
+        for line in in_file.readlines():
+            arr.append(float(line))
+
+        plot_points.append(float(np.average(arr)))
+    plot.plot(iterations, plot_points, '.c-')
+    plot.xscale('log')
+plot_points = []
+iterations = []
+
+
+plot.show()
--- script.sh
+++ script.sh
@@ -20,21 +20,15 @@
 set -o nounset                              # Treat unset variables as an error
 
 declare -a ITERS=(100 1000 10000 100000 1000000 10000000 100000000)
-declare -a COUNTS=("hundred" "thousand" "tenK" "hundredK" "mill" "tenMill" "hundredMill")
 
 algos=4
-limit=10 # How many run we want to do
-index=0
+runs=10 # How many run we want to do
 
 for a in $(seq 1 $algos); do
-  echo "algo $a"
   for i in "${ITERS[@]}"; do
-    echo $index
-    for k in $(seq 1 $limit); do
-      node --max-old-space-size=4096 ./index.js "$i" "$a" >> case_"$a"_count_"${COUNTS[$index]}"
+    for run in $(seq 1 $runs); do
+      node --max-old-space-size=4096 ./index.js "$i" "$a" >> case_"$a"_count_"$i"
     done
-    ((index++))
   done
-  index=0
 done
 
